5f768d420c64ae5514b120ad8e09501c27613a77
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-04-19 21:47:49 -0400

    Style PersonalInfo component, adjust App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import PersonalInfo from "./components/PersonalInfo"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function App() {
 
@@ -8,7 +7,20 @@ function App() {
       <section className="CV-builder">
         <PersonalInfo />
       </section>
-      <section className="CV"></section>
+      <section className="CV">
+        <div className="info">
+          <div className="name">
+            <h1>First Name<br />Last Name</h1>
+            <p><i>Software Developer</i></p>
+          </div>
+          <div className="contactInfo">
+            <p className="bold">Address:</p> <p>123 Main St New York, NY 00000</p>
+            <p className="bold">Email: </p><a href="mailto:email@email.com">email@email.com</a>
+            <p className="bold">Phone: </p><p>(000) 000-00000</p>
+            <p className="bold">Github: </p><a href="https://github.com/" target="_blank">https://github.com/</a>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons"
+import styles from '../styles/components.module.css'
 
 function PersonalInfo () {
   return(
-    <div>
-      <FontAwesomeIcon icon={faAddressCard} />
-      <h1>Personal Info</h1>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <FontAwesomeIcon icon={faAddressCard} />
+        <h1>Personal Info</h1>
+      </div>
+      <div className={styles.doubleInput}>
         <input placeholder="First Name"/>
         <input placeholder="Last Name"/>
       </div>
@@ -16,7 +19,7 @@ function PersonalInfo () {
       <div>
         <input placeholder="Address"/>
       </div>
-      <div>
+      <div className={styles.doubleInput}>
         <input placeholder="Phone Number"/>
         <input placeholder="Email"/>
       </div>
