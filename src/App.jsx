340a2fec414ef2b42eb110234a863a20b2eed9f2
Af1991 <152735889+Afer1991@users.noreply.github.com>
2025-04-20 15:53:07 -0400

    Add functionality to components to display personal info

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,74 @@
 import PersonalInfo from "./components/PersonalInfo"
+import { useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState('First Name');
+  const [lastName, setLastName] = useState('Last Name');
+  const [title, setTitle] = useState('Software Developer');
+  const [address, setAddress] = useState('123 Main St New York, NY 00000');
+  const [phoneNumber, setPhoneNumber] = useState('(000) 000-00000');
+  const [email, setEmail] = useState('email@email.com');
+  const [gitHub, setGitHub] = useState('');
+
+  function firstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function lastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function titleChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function addressChange(e) {
+    setAddress(e.target.value);
+  }
+
+  function phoneNumberChange(e) {
+    setPhoneNumber(e.target.value);
+  }
+
+  function emailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function gitHubChange(e) {
+    setGitHub(e.target.value);
+  }
 
   return (
     <>
       <section className="CV-builder">
-        <PersonalInfo />
+        <PersonalInfo 
+          updateFirstName={firstNameChange}
+          updateLastName={lastNameChange}
+          updateTitle={titleChange}
+          updateAddress={addressChange} 
+          updatePhoneNumber={phoneNumberChange}
+          updateEmail={emailChange}
+          updateGitHub={gitHubChange}
+        />
       </section>
       <section className="CV">
         <div className="info">
           <div className="name">
-            <h1>First Name<br />Last Name</h1>
-            <p><i>Software Developer</i></p>
+            <h1>{firstName}<br />{lastName}</h1>
+            <p><i>{title}</i></p>
           </div>
           <div className="contactInfo">
-            <p className="bold">Address:</p> <p>123 Main St New York, NY 00000</p>
-            <p className="bold">Email: </p><a href="mailto:email@email.com">email@email.com</a>
-            <p className="bold">Phone: </p><p>(000) 000-00000</p>
-            <p className="bold">Github: </p><a href="https://github.com/" target="_blank">https://github.com/</a>
+            <p className="bold">Address:</p> <p>{address}</p>
+            <p className="bold">Phone: </p><p>{phoneNumber}</p>
+            <p className="bold">Email: </p><a href={"mailto:" + email}>{email}</a>
+            {gitHub !== '' ? 
+              (
+                <>
+                  <p className="bold">Github: </p><a href={"https://github.com/" + gitHub} target="_blank">{"https://github.com/" + gitHub}</a>
+                </>
+              ) :
+              null
+            }
           </div>
         </div>
       </section>
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -2,7 +2,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons"
 import styles from '../styles/components.module.css'
 
-function PersonalInfo () {
+function PersonalInfo ({
+  updateFirstName,
+  updateLastName,
+  updateTitle,
+  updateAddress,
+  updatePhoneNumber,
+  updateEmail,
+  updateGitHub
+}) {
   return(
     <div className={styles.container}>
       <div className={styles.heading}>
@@ -10,21 +18,21 @@ function PersonalInfo () {
         <h1>Personal Info</h1>
       </div>
       <div className={styles.doubleInput}>
-        <input placeholder="First Name"/>
-        <input placeholder="Last Name"/>
+        <input placeholder="First Name" onChange={updateFirstName} />
+        <input placeholder="Last Name" onChange={updateLastName} />
       </div>
       <div>
-        <input placeholder="Title"/>
+        <input placeholder="Title" onChange={updateTitle}/>
       </div>
       <div>
-        <input placeholder="Address"/>
+        <input placeholder="Address" onChange={updateAddress}/>
       </div>
       <div className={styles.doubleInput}>
-        <input placeholder="Phone Number"/>
-        <input placeholder="Email"/>
+        <input placeholder="Phone Number" onChange={updatePhoneNumber} />
+        <input placeholder="Email" onChange={updateEmail}/>
       </div>
       <div>
-        <input placeholder="Github Link (optional)"/>
+        <input placeholder="Github Link (optional)" onChange={updateGitHub} />
       </div>
     </div>
   );
